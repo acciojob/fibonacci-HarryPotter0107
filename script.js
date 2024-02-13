7340f84af98b8de0304536ffc830cf213ea83379
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-14 01:48:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function fibonacci(num) {
 // your code here
+	let a = 0
+	let b = 1;
+	// let c = a + b;
+	let c;
+
+	for(let i = 2; i < n; i++ ){
+		c = a + b;
+		a = b;
+		b = c;
+	}
+	return c;
 }
 
 module.exports = fibonacci;
