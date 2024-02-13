6fab49f450014985c5a3d8cccaced3ecdba75290
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-14 02:22:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,24 @@
+
+
 function fibonacci(num) {
-// your code here
-	if(num === 1) return 0;
-	if(num === 2) return 1;
-	let prev = 0
-	let curr = 1;
-	
+  if (num === 1 ) {
+    return 0;
+  }
+   if (num === 2 ) {
+    return 1;
+  }
 
-	for(let i = 3; i <= num; i++ ){
-		let next = prev + curr;
-		prev = curr;
-		curr = next;
-	}
+  var num1 = 0;
+  var num2 = 1;
 
-	return next;
-	// console.log(c);
+  for (var i = 3; i <= num; i++) {
+    var result = num1 + num2;
+    num1= num2;
+    num2 = result;
+  }
+
+  return result;
 }
-
-// function fibonacci(num) {
-//   if (num === 1 ) {
-//     return 0;
-//   }
-//    if (num === 2 ) {
-//     return 1;
-//   }
-
-//   var num1 = 0;
-//   var num2 = 1;
-
-//   for (var i = 3; i <= num; i++) {
-//     var result = num1 + num2;
-//     num1= num2;
-//     num2 = result;
-//   }
-
-//   return result;
-// }
 
 
 module.exports = fibonacci;
