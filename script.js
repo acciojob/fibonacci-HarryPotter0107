597cf90502ff3b89438a3240204908898a329d5d
HarryPotter0107 <132452554+HarryPotter0107@users.noreply.github.com>
2024-02-14 02:13:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,40 +1,40 @@
-// function fibonacci(num) {
-// // your code here
-// 	if(num == 0) return 0;
-// 	if(num == 1) return 1;
-// 	let prev = 0
-// 	let curr = 1;
-// 	let next;
-
-// 	for(let i = 2; i <= num; i++ ){
-// 		next = prev + curr;
-// 		prev = curr;
-// 		curr = next;
-// 	}
-
-// 	return prev;
-// 	// console.log(c);
-// }
-
 function fibonacci(num) {
-  if (num === 1 ) {
-    return 0;
-  }
-   if (num === 2 ) {
-    return 1;
-  }
+// your code here
+	if(num == 1) return 0;
+	if(num == 2) return 1;
+	let prev = 0
+	let curr = 1;
+	let next;
 
-  var num1 = 0;
-  var num2 = 1;
+	for(let i = 2; i <= num; i++ ){
+		next = prev + curr;
+		prev = curr;
+		curr = next;
+	}
 
-  for (var i = 3; i <= num; i++) {
-    var result = num1 + num2;
-    num1= num2;
-    num2 = result;
-  }
-
-  return result;
+	return next;
+	// console.log(c);
 }
+
+// function fibonacci(num) {
+//   if (num === 1 ) {
+//     return 0;
+//   }
+//    if (num === 2 ) {
+//     return 1;
+//   }
+
+//   var num1 = 0;
+//   var num2 = 1;
+
+//   for (var i = 3; i <= num; i++) {
+//     var result = num1 + num2;
+//     num1= num2;
+//     num2 = result;
+//   }
+
+//   return result;
+// }
 
 
 module.exports = fibonacci;
